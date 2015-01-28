9c0e019a9a17d7028a12a7df62d9e31b317b6dec
Jason Whitehouse <jdb175@gmail.com>
2015-01-28 13:16:09 -0500

    Added polygon fill and axes to population chart:

--- BaseGame.js
+++ BaseGame.js
@@ -3,6 +3,8 @@ var rows = 40;
 var columns = 40;
 var width = 600;
 var height = 800;
+var sideMargin = 25;
+var betweenMargin = 10;
 var canvas;
 
 //State info
@@ -34,8 +36,8 @@ window.onload = function () {
 	SetInitialState();
 
 	canvas = d3.select("svg")
-		.attr("width", width)
-		.attr("height", height)
+		.attr("width", width + 2*sideMargin)
+		.attr("height", height + 2*sideMargin + betweenMargin)
 		.on('click', function() {svgPaint(this)})
 		.on('mouseover', function() {if(mouseDown) {svgPaint(this)}});
 
@@ -71,6 +73,7 @@ window.onload = function () {
   	});
 
 	showGrid();
+	showPopulationGraph();
 }
 
 function reset() {
@@ -81,12 +84,7 @@ function reset() {
 	document.getElementById("Population").innerHTML = population;
 	document.getElementById("Generation").innerHTML = generation;
 	showGrid();
-}
-
-function placeAt(obj, row, column) {
-	var index = (row*rows) + column;
-	curGeneration[index] = obj;
-	showGrid();
+	showPopulationGraph();
 }
 
 function beginSimulation() {
@@ -133,6 +131,7 @@ function simulate() {
 		document.getElementById("Population").innerHTML = population;
 		curGeneration = nextData;
 		showGrid();
+		showPopulationGraph();	
 	}
 }
 
@@ -148,22 +147,24 @@ function showGrid(){
 		.attr("cy", function(d, i) {
 			var row = Math.floor(i/rows);
 			var column = i - row*rows;
-	        return column*width/rows+width/rows/2;
+	        return sideMargin + column*width/rows+width/rows/2;
 		})
 		.attr("cx", function(d, i) {
 			var row = Math.floor(i/rows);
 			var column = i - row*rows;
-	        return row*width/rows+width/rows/2;
+	        return sideMargin +row*width/rows+width/rows/2;
 		})
 		.attr("fill", function(d) { return getColor(d); } );
 
 	data.transition().duration(100).attr("fill", function(d) { return getColor(d); } )
+}
 
+function showPopulationGraph() {
 	//Show population chart
 	//handle scales
 	//We want to scale from 0 to the max population on y
 	var maxPop = Math.max.apply(Math,prevGenerations.map(function(o){return o.population;}));
-	var yScale = d3.scale.linear().domain([0, maxPop]).range([height, width]);
+	var yScale = d3.scale.linear().domain([0, Math.max(10, maxPop)]).range([height+betweenMargin+sideMargin, width+betweenMargin+sideMargin]);
 
 	//and across our stored generations for x, without scaling across when we have stored less than max
 	var startGen = 0;
@@ -171,10 +172,32 @@ function showGrid(){
 		startGen = prevGenerations[0].number;
 	}
 	var endGen = startGen + maxGenerationsStored;
+	var xScale = d3.scale.linear().domain([startGen, endGen]).range([sideMargin, width+sideMargin]);
 
-	var xScale = d3.scale.linear().domain([startGen, endGen]).range([0, width]);
+	//Handle Axes
+	var xAxis = d3.svg.axis()
+		.scale(xScale)
+		.orient("bottom");
+	var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("right");
 
-	//create a line
+	//Create an SVG group Element for the Axis elements and add axes
+	canvas.selectAll("g")
+		.remove();
+	canvas.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0, "+(height+sideMargin+betweenMargin)+")")
+		.call(xAxis);
+	canvas.append("g")
+		.attr("transform", "translate("+(width+sideMargin)+", 0)")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	    .selectAll("line")
+	    .attr("x1", -(width))
+	    .attr("stroke-dasharray", "2,2");
+
+	//Now draw the line(s)
 	var line =d3.svg.line()
 		.x(function(d,i) { return xScale(d.number) })
 		.y(function(d,i) { return yScale(d.population) });
@@ -186,16 +209,34 @@ function showGrid(){
 		afterSelection = prevGenerations.slice(selectedGen-1);
 	}
 	//add it
-	canvas.select("#afterLine")
-			.attr('fill', 'none')
-			.attr("stroke-width",2)
-			.attr("stroke", "gray")
-			.attr("d", line(afterSelection));	
-	canvas.select("#line")
-			.attr('fill', 'none')
-			.attr("stroke-width",2)
-			.attr("stroke", "black")
-			.attr("d", line(beforeSelection));		
+	var group = canvas.append('g')
+				.attr('id', 'populationLine');
+
+	group.append('path')
+		.attr('id', '#afterLine')
+		.attr('fill', 'lightblue')
+		.attr("stroke-width",2)
+		.attr("stroke", "gray")
+		.attr("d", line(processDataforPolygon(afterSelection)));	
+	group.append('path')
+		.attr('id', '#beforeLine')
+		.attr('fill', 'blue')
+		.attr("stroke-width",2)
+		.attr("stroke", "black")
+		.attr("d", line(processDataforPolygon(beforeSelection)));
+}
+
+//create a polygon dataset from data (adding points to fill shape)
+function processDataforPolygon (d) {
+	if(d.length == 0)
+		return [];
+	poly = d.slice(0);
+	//Add a copy of the first and last points with y=0
+	var first = new savedState(poly[0].state, poly[0].number, 0);
+	var last = new savedState(poly[poly.length-1].state, poly[poly.length-1].number, 0);
+	poly.unshift(first);
+	poly.push(last);
+	return poly;
 }
 
 function applySavedState(index) {
@@ -207,19 +248,24 @@ function applySavedState(index) {
 	document.getElementById("Generation").innerHTML = generation;
 	document.getElementById("Population").innerHTML = population;
 	showGrid();
+	showPopulationGraph();
 	stopSim = true;
 }
 
 //Handles clicking at top level
 function svgPaint (o) {
 	var mPos = d3.mouse(o);
+	mPos[0] -= sideMargin;
+	mPos[1] -= sideMargin;
 	if (mPos[0] > width) {
 		return;
-	} else if(mPos[1] > width) {
+	} else if(mPos[1] > width+betweenMargin && mPos[1] < height) {
 		//Handle generation switching
 		var generationS = Math.round(mPos[0]/width*maxGenerationsStored);
 		if(generationS < prevGenerations.length) {
 			applySavedState(generationS);
+		} else if(selectedGen != null) {
+			applySavedState(prevGenerations[prevGenerations.length-1].number);
 		}
 	} else {
 		var row = Math.round(mPos[0]/width*rows - 0.5);
